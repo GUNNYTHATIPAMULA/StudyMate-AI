fc7ce3451fb591385de63f43bd172391ab68a22e
GUNNYTHATIPAMULA <gunnysmartz@gmail.com>
2025-09-07 17:25:50 +0530

    modified vercel link

--- Backend/main.py
+++ Backend/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_origins=[               # local dev
         "https://study-mate-ai-sigma.vercel.app/",
         
-        "http://localhost:3000",# deployed frontend on Vercel
+        "http://localhost:3001",# deployed frontend on Vercel
     ],
     allow_credentials=True,
     allow_methods=["*"],
